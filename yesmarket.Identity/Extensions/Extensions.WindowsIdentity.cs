7d04574f33b2a1a7ab80ebc2c6e5c1daf5e5e50d
Ryan Bartsch <rbartsch@homestart.com.au>
2015-09-03 15:37:53 +0930

    added NameWithoutDomain WindowsIdentity extension

---
+++ yesmarket.Identity/Extensions/Extensions.WindowsIdentity.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace yesmarket.Identity.Extensions
+{
+    public static class WindowsIdentityExtensions
+    {
+        public static string NameWithoutDomain(this WindowsIdentity value)
+        {
+            return value != null ? value.Name.Split('\\')[1] : string.Empty;
+        }
+    }
+}
