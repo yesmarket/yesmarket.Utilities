3d592a23be933f1d4bfc803b44761b05ab85e7f1
Ryan Bartsch <rbartsch@homestart.com.au>
2015-09-10 11:16:30 +0930

    added ToFault extension method

---
+++ yesmarket.WCF/Extensions/Exception.Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using yesmarket.Wcf.FaultContracts;
+
+namespace yesmarket.Wcf.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static Fault ToFault(this Exception value)
+        {
+            return new Fault(value.Message);
+        }
+    }
+}
